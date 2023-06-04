6852aa965dc210d48d6689811601b3c02e4c5248
Nikita <niksfarisey@yandex.ru>
2023-06-04 02:22:31 +0200

    initial commit

---
+++ StorageService/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  StorageService
+//
+//  Created by Никита on 04.06.2023.
+//
+
+import Foundation
+
+struct Post {
+    let author: String
+    let description: String
+    let image: String
+    let likes: Int
+    let views: Int
+}
